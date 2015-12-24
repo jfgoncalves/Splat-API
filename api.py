00ee2698f7261f79bec10a46e1ab9b8ba209a56c
Jean-François Goncalves <jfgoncalves@me.com>
2015-12-24 10:56:20 +0100

    Update fetching method
    POST is needed

--- api.py
+++ api.py
@@ -3,15 +3,39 @@
 import requests
 import json
 
+#based on https://gist.github.com/anonymous/9c790980b4955a35187a
+
+NINTENDO_LOGIN_PAGE = "https://id.nintendo.net/oauth/authorize"
+
+SPLATNET_CALLBACK_URL = "https://splatoon.nintendo.net/users/auth/nintendo/callback"
+SPLATNET_CLIENT_ID = "12af3d0a3a1f441eb900411bb50a835a"
+
 SPLATNET_SCHEDULE_URL = "https://splatoon.nintendo.net/schedule/index.json?utf8=✓"
 
-#Set this with the _wag_session value of your cookie, by logging with your credentials before.
+#Set this with your credentials
 
-wagEU = ''
-wagNA = ''
-wagJP = ''
+username_eu = ''
+password_eu = ''
+username_na = ''
+password_na = ''
+username_jp = ''
+password_jp = ''
 
 #based on https://github.com/Wiwiweb/SakuraiBot/blob/master/src/sakuraibot.py
+
+def get_new_splatnet_cookie(u, pwd):
+    parameters = {'client_id': SPLATNET_CLIENT_ID,
+                  'response_type': 'code',
+                  'redirect_uri': SPLATNET_CALLBACK_URL,
+                  'username': u,
+                  'password': pwd}
+
+    response = requests.post(NINTENDO_LOGIN_PAGE, data=parameters)
+
+    cookie = response.history[-1].cookies.get('_wag_session')
+    if cookie is None:
+        raise Exception("Couldn't retrieve cookie")
+    return cookie
 
 def get_splatnet_schedule(splatnet_cookie):
     cookies = {'_wag_session': splatnet_cookie}
@@ -19,11 +43,9 @@ def get_splatnet_schedule(splatnet_cookie):
     response = requests.get(SPLATNET_SCHEDULE_URL, cookies=cookies, data={'locale':"ja"})
     status = response.status_code
 
-    #Check if splatoon.nintendo.net is under maintenance
+    #Check if splatoon.nintendo.net is ready
 
-    if status == 503:
-        data = status
-    else:
+    if status == 200:
         data = response.json()
 
         schedule = data["schedule"]
@@ -53,20 +75,24 @@ def get_splatnet_schedule(splatnet_cookie):
             else:
                 for map in rotation["stages"]:
                     del map["asset_path"]
-
-    return data
-
-def write_schedule(splatnet_cookie, region):
-    dataI = get_splatnet_schedule(splatnet_cookie)
-    if dataI == 503:
-        pass
     else:
+        data = None
+
+    return (status, data)
+
+def write_schedule(u, pwd, region):
+    splatnet_cookie = get_new_splatnet_cookie(u, pwd)
+    statusI, dataI = get_splatnet_schedule(splatnet_cookie)
+
+    if statusI == 200:
         with open('/YOUR/PATH/TO/schedule_'+region+'.json', 'w', encoding='utf-8') as outfile:
             json.dump(dataI, outfile, ensure_ascii=False)
+    else:
+        pass
 
 
 if __name__ == "__main__":
 
-    write_schedule(wagEU, "eu")
-    write_schedule(wagEU, "na")
-    write_schedule(wagEU, "jp")
+    write_schedule(username_eu, password_eu, "eu")
+    write_schedule(username_na, password_na, "na")
+    write_schedule(username_jp, password_jp, "jp")
